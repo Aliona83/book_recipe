f8f7f54bc3a6afb7e6203a79e5ac988552b34d63
Aliona83 <alionajanusko@gmail.com>
2023-03-24 14:49:18 +0000

    Add form for sing up

--- recipebook/bookapp/views.py
+++ recipebook/bookapp/views.py
@@ -1,14 +1,16 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 
 
 def index(request):
+    form = UserCreationForm()
     return render(request, "bookapp/index.html")
 
 
 def signup(request):
-    return render(request, "registration/signup.html")
+    return render(request, "registration/signup.html", {"form": form})
 
 
 def home(request):
