cf4585dde3b9b9f4f6acbd33fb5f4abd499a90c2
Aliona83 <alionajanusko@gmail.com>
2023-03-31 10:09:01 +0000

    Add new file and function for register_user

--- recipebook/members/views.py
+++ recipebook/members/views.py
@@ -1,18 +1,29 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
 from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm
 
 
 def login_user(request):
+    
     if request.method == "POST":
-        username = request.POST('username')
-        password = request.POST('password')
+        username = request.POST['username']
+        password = request.POST['password']
+
         user = authenticate(request, username=username, password=password)
-    if user is not None:
-        login(request, user)
-        return redirect('recipe_list')
-    else:
-        messages.success(request, "There Was An Error Logging In, Try Again")
-        return redirect('login')
-        pass
-        return render(request, 'authenticate/login.html', {})
+
+        if user is not None:
+            login(request, user)
+            return redirect('recipe_list')
+        else: 
+            return render(request, 'authenticate/login.html', {'user':user})
+
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, ("yOU SDBCJHDBJ"))
+    return redirect('home')
+
+
+def register_user(request):
+    return render(request, 'authenticate/registration_user.html')  
