ee0aad40994b78c7a87881aaa342bd37b54c06e6
Aliona83 <alionajanusko@gmail.com>
2023-04-03 11:37:01 +0000

    Create authentication system for signup page

--- recipebook/members/views.py
+++ recipebook/members/views.py
@@ -3,21 +3,24 @@ from django.http import HttpResponse
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.contrib.auth.forms import UserCreationForm
-
+from django.contrib.auth.models import User
 
 def login_user(request):
     if request.method == "POST":
         username = request.POST.get('username')
-        password = request.POST.get('password')
+        password = request.POST.get('pass1')
 
-        user = authenticate(request, username=username, password=password)
+        myuser = authenticate(request, username=username, password=pass1)
     if user.is_authenticated:
         return HttpResponse(f"You are already authenticated as {request.user}.")
-    if user is not None:
-        login(request, user)
-        return redirect('login')
+    if myuser is not None:
+        login(request, myuser)
+        message.success(request, "Login success")
+        return redirect('/')
     else: 
+        message.error(request, "Invalid Credentails")
         return render(request, 'authenticate/login.html')
+    return render(request, 'login.html')   
 
 
 def logout_user(selfrequest):
@@ -27,21 +30,35 @@ def logout_user(selfrequest):
 
 
 def register_user(request):
-    user = request.user
-    if user.is_authenticated:
-        return HttpResponse(f"You are already authenticated as {request.user}.")
-        context = {'form':form}
+    if request.method == "POST":
+        username = request.POST.get("username")
+        email = request.POST.get("email")
+        password = request.POST.get("pass1")
+        confirmpassword = request.POST.get("pass2")
+        if password != confirmpassword:
+            message.warning(request, "Password is Incorrect")
+            return redirect('/register_user')
 
-        form = UserCreationForm(request.POST)
+        try:
+            if User.objects.get(username=username):
+                message.info(request, "Username is taken")
+                return redirect('/register_user')
+        except:
+            pass
+        try:
+            if User.objects.get(email=email):
+                messages.info(request, "Email Is Taken")
+                return redirect('/register_user')
+        except:
+            pass
+    
+        myuser=User.objects.create_user(username, email, password)
+        myuser.save()
+        messages.success(request, "Signup Success Please login!")
+        return redirect('/login')
+              
+    return render(request, 'register_user.html')
         
-        if form.is_valid():
-            form.save()
-            username = form.cleaned_data['username']
-            password = form.cleaned_data['password1']
-            user = authenticate(username=username, password=password)
-            login = (request, user)
-            messages.success(request, ("Registration seccesfull"))
-            return redirect('recipe_list')
-        else:
-            form = UserCreationForm() 
-    return render(request, 'authenticate/register_user.html', {})  
+
+    
+        
